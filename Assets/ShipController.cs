c90f5342397db1eb23f9fe14342cf7cbee206c7c
Samuel Hegner <C16439262@mydit.ie>
2019-03-26 09:35:43 +0000

    Stuff

---
+++ Assets/ShipController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekState: State{
+    public override void Enter(){
+    }
+}
+
+
+public class ShipController : MonoBehaviour
+{
+    public GameObject bullet;
+
+    GameObject targetBase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShipSetup(Color col, GameObject target){
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            r.material.color = col;
+        }
+
+        targetBase = target;
+    }
+}
