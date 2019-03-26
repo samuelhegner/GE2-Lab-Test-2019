a8e567a188a210b1843aefd433e5698d5ce37b47
Samuel Hegner <C16439262@mydit.ie>
2019-03-26 09:34:31 +0000

    Ships spawn with target and colour

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -11,17 +11,30 @@ public class Base : MonoBehaviour
 
     public GameObject fighterPrefab;
 
+    Color colToAssign;
+
+    public List<GameObject> otherBases = new List<GameObject>();
+
 
     // Start is called before the first frame update
     void Start()
     {
+        colToAssign = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
         foreach (Renderer r in GetComponentsInChildren<Renderer>())
         {
-            r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+            r.material.color = colToAssign;
+            
         }
-        StartCoroutine(MineTiberium());
 
-        
+        GameObject[] _allBases = GameObject.FindGameObjectsWithTag("base");
+
+        foreach(GameObject _base in _allBases){
+            if(_base != gameObject){
+                otherBases.Add(_base);
+            }
+        }
+
+        StartCoroutine(MineTiberium());
     }
 
     // Update is called once per frame
@@ -42,7 +55,8 @@ public class Base : MonoBehaviour
     }
 
     void SpawnShip(){
-        Instantiate(fighterPrefab, transform.position, Quaternion.identity);
+        GameObject newShip = Instantiate(fighterPrefab, transform.position, Quaternion.identity);
+        newShip.GetComponent<ShipController>().ShipSetup(colToAssign, otherBases[Random.Range(0, 3)]);
         tiberium -= 10;
     }
 
